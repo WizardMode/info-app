62077cf4e49a9d265b706f0963d23e6dc8da42be
WizardMode <humanspacebolt@live.com>
2022-03-22 17:48:58 -0700

    main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@ import React from "react"
 
 function Main() {
     return (
-        <h1>Main goes here</h1>
+        <main className="main">
+            <h1 className="main-title">Fun facts about React</h1>
+            <ul className="main-facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
